71300c865704ee43de0373b8d50528479cc25866
Camila <camilaferno1999@gmail.com>
2021-10-27 12:54:09 -0400

    Part A-Product List

---
+++ product_manager/delete.php
@@ -0,0 +1,26 @@
+<?php
+  //Produced by: Camila Fernandez Noguchi
+
+  $con = mysqli_connect("webdev.bentley.edu", "fernandcami", "2303", "fernandcami");
+
+  if (mysqli_connect_errno($con)) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error() . "<br>";
+        exit("Connect Error");
+    }
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $id = $_POST['deleteItem'];
+      $query = "DELETE FROM PRODUCTS WHERE PRODUCTCODE = '$id'";
+
+      if (mysqli_query($con, $query)) {
+        #echo "Record deleted successfully";
+        header('Location: index.php');
+      }
+      else {
+        echo "Error deleting record: " . mysqli_error($con);
+      }
+  }
+
+  mysqli_close($con);
+
+?>
--- product_manager/index.php
+++ product_manager/index.php
@@ -1,7 +1,62 @@
-<?php
-require('../model/database.php');
+<?php require('../view/header.php');?>
 
+<main>
+    <!--Produced by: Camila Fernandez Noguchi-->
+	<h1>Product List</h1>
 
-include('../under_construction.php');
+	<table>
+	<?php
 
-?>
+	# Conect to database
+	$con = mysqli_connect("webdev.bentley.edu", "fernandcami", "2303", "fernandcami");
+
+	# Check connection
+	if (mysqli_connect_errno($con)) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error() . "<br>";
+        exit("Connect Error");
+    }
+
+    # Perform SQL query
+    $query = "SELECT * FROM products;";
+    $result = mysqli_query($con, $query) or die('Query failed: ' . mysqli_errno($con));
+
+    # field result set
+    $finfo = mysqli_fetch_fields($result);
+    # loop through field names and print as headers
+    echo "<tr>";
+    foreach ($finfo as $val) {
+	echo "<th>" . " $val->name" . "</th>";
+	}
+	echo "<th></th>"; # empty header for delete button
+	echo "</tr>";
+
+    # loop over result set. Print field values for each record
+    while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        # start table row
+        echo "<tr>";
+
+        # inner loop. Print each field value for a record
+        foreach ($line as $field_value) {
+            echo "<td>", "$field_value", "</td>";
+        }
+
+        #delete buttons
+       	echo "<form action='delete.php' method='post'>";
+        echo "<td> <button type='submit' name='deleteItem' value='" . $line['productCode'] . "' />Delete</button></td>";
+        echo "</form>";
+
+        # end table row and loop
+        echo "</tr>";
+    }
+
+    # close connection
+    mysqli_close($con);
+
+	?>
+	</table>
+
+    <a href="addProduct.php">Add Product</a>
+
+</main>
+
+<?php include('../view/footer.php');?>
