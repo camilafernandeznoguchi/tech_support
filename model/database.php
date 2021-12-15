f7155dd944b848794d5e83cc81cc9d25aff3c0ef
Camila Fernandez Noguchi <93275830+camilafernandeznoguchi@users.noreply.github.com>
2021-12-14 19:23:43 -0500

    fixed password for db

--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
     $username = 'fernandcami';
-    $psw = '0323';
+    $psw = '2303';
 
 	// Connect to MySQL, select database
 	$con = mysqli_connect('webdev.bentley.edu', $username, $psw, $username);
